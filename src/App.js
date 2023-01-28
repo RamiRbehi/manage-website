9dcad779c503cce64b9839d88a3c07b5a691fe43
RamiRbehi <ramirbehi92@gmail.com>
2023-01-28 14:10:46 +0100

    add testimonials component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './Components/About';
 import Intro from './Components/Intro';
 import Navbar from './Components/Navbar';
+import Testimonials from './Components/Testimonials';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Intro/>
       <About/>
+      <Testimonials/>
     </div>
   );
 }
